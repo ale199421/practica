ac68ec07d19dd9e51ef4961ee0667236edc28644
Alejandra Carreño <52646044+ale199421@users.noreply.github.com>
2019-12-08 22:59:17 -0500

    rev 10

---
+++ pratica.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="es-ES">
+<head>
+	<meta charset="UTF-8">
+	<title>Suma de 2 numeros en PHP</title>
+</head>
+<body>
+<H2>Suma de 2 numeros en PHP</H2>
+<form action="suma.php" method="POST">
+	<table>
+	<tr>
+		<td><input type="text" name="numero1"></td>
+	</tr>
+	<tr>
+		<td><input type="text" name="numero2"></td>
+	</tr>
+	<tr>
+		<td> <input type="submit" value="sumar"> </td>
+	</tr>
+</table>
+</form>
+</body>
+</html>
---
+++ suma.php
@@ -0,0 +1,14 @@
+<?php
+	if($_POST)
+	{	
+		$num1 = $_POST
+		['numero1'];
+		$num2 = $_POST
+		['numero2'];
+		$suma = $num1 
+		+ $num2;
+	echo "hola1 ";
+echo "hola3 ";
+		echo "La suma de ".$num1." y ".$num2." es ".$suma; 
+	}
+?>
